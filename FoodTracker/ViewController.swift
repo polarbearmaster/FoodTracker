1056ac5b48b270e65613e210d9d000dd128849d8
polarbearmaster <kennyhuang34@hotmail.com>
2016-05-02 16:39:08 +0800

    basic-UI

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func setDefaultLabeltext(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 
 }
 
