7ce06480e06914aeff921cc6bcadae5eb9dee209
polarbearmaster <kennyhuang34@hotmail.com>
2016-05-02 20:29:40 +0800

    Work with View Controllers

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by kenny on 5/2/16.
+//  Copyright © 2016 kenny. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     
+    @IBOutlet weak var photoImageVIew: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         nameTextField.delegate = self
@@ -32,5 +33,24 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mealNameLabel.text = "Default Text"
     }
 
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .PhotoLibrary
+        imagePickerController.delegate = self
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
+    // MARK: UIImagePickerCOntrollerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoImageVIew.image = selectedImage
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
