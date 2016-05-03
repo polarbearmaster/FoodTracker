4b75217d97e61549caaec6dca7cb6d3c3a54be61
Kenny Huang <Fujun.Huang@Honeywell.com>
2016-05-03 13:25:19 +0800

    Define your data model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,29 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by kenny on 16/5/3.
+//  Copyright © 2016年 kenny. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+    
+}
