8f2f71556d66a20711c21cd40a545ad0a4c7c53a
Kenny Huang <Fujun.Huang@Honeywell.com>
2016-05-05 11:09:18 +0800

    Implement Navigation

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -15,9 +15,21 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     @IBOutlet weak var photoImageVIew: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    /*
+ This value is either passed by 'MealTableViewController' in 'prepareForSegue(_:sender:)' or constructed as part of adding a new meal.
+ */
+    var meal: Meal?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Handle the text field's user input through delegate callbacks
         nameTextField.delegate = self
+        
+        // Enable the Save button only if the text field has a valid Meal name
+        checkValidMealName()
     }
     
     // MARK: UITextFieldDelegate
@@ -26,6 +38,32 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         return true
     }
     func textFieldDidEndEditing(textField: UITextField) {
+        checkValidMealName()
+        navigationItem.title = textField.text
+    }
+    func textFieldDidBeginEditing(textField: UITextField) {
+         saveButton.enabled = false
+    }
+    func checkValidMealName() {
+        // Disable the Save button if the text field is empty
+        let text = nameTextField.text ?? ""
+        saveButton.enabled = !text.isEmpty
+    }
+    
+    // MARK: Navigation
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if saveButton === sender {
+            let name = nameTextField.text ?? ""
+            let photo = photoImageVIew.image
+            let rating = ratingControl.rating
+            
+            meal = Meal(name: name, photo: photo, rating: rating)
+            
+        }
     }
     
     // MARK: Actions
